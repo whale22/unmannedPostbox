2c2df97461790c9bd3c93b8c9b1a7f0fc81a6480
whale22 <grue22o_oa@naver.com>
2018-05-01 21:55:42 +0900

    전체적인 기능 추가(NFC, 서버 접속)

--- app/src/main/java/com/example/user/unmannedpostbox/NFCActivity.java
+++ app/src/main/java/com/example/user/unmannedpostbox/NFCActivity.java
@@ -3,9 +3,12 @@ package com.example.user.unmannedpostbox;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.nfc.NfcAdapter;
+import android.widget.Toast;
 
 public class NFCActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -14,6 +17,18 @@ public class NFCActivity extends AppCompatActivity implements View.OnClickListen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nfc);
         findViewById(R.id.cancel_action).setOnClickListener(this);
+
+        // NFC 관련 객체 생성
+        NfcAdapter mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if (!mNfcAdapter.isEnabled())
+        {
+            Toast.makeText(getApplicationContext(), "NFC가 꺼져있습니다. 안드로이드 설정에서 NFC설정을 ON으로 해 주세요.", Toast.LENGTH_LONG).show();
+            startActivity(new Intent(android.provider.Settings.ACTION_NFC_SETTINGS));
+        }
+        else{
+            startActivity(new Intent(this,OpenCloseActivity.class));
+            this.finish();
+        }
     }
     public void onClick(View v) {
         switch (v.getId()) {
---
+++ app/src/main/java/com/example/user/unmannedpostbox/OpenCloseActivity.java
@@ -0,0 +1,93 @@
+package com.example.user.unmannedpostbox;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TabHost;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class OpenCloseActivity extends AppCompatActivity implements View.OnClickListener {
+    // 전역변수를 선언한다
+    String myId, myPWord, myTitle, mySubject, myResult;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_open_close);
+        findViewById(R.id.control_action).setOnClickListener(this);
+        findViewById(R.id.cancel_action).setOnClickListener(this);
+    }
+
+    //------------------------------
+    //   Http Post로 주고 받기
+    //------------------------------
+    public void HttpPostData() {
+        try {
+            //--------------------------
+            //   URL 설정하고 접속하기
+            //--------------------------
+            URL url = new URL("192.168.0.1");       // URL 설정
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();   // 접속
+            //--------------------------
+            //   전송 모드 설정 - 기본적인 설정이다
+            //--------------------------
+            http.setDefaultUseCaches(false);
+            http.setDoInput(true);                         // 서버에서 읽기 모드 지정
+            http.setDoOutput(true);                       // 서버로 쓰기 모드 지정
+            http.setRequestMethod("POST");         // 전송 방식은 POST
+
+            // 서버에게 웹에서 <Form>으로 값이 넘어온 것과 같은 방식으로 처리하라는 걸 알려준다
+            http.setRequestProperty("content-type", "application/x-www-form-urlencoded");
+            //--------------------------
+            //   서버로 값 전송
+            //--------------------------
+            /*
+            StringBuffer buffer = new StringBuffer();
+            buffer.append("id").append("=").append(myId).append("&");                 // php 변수에 값 대입
+            buffer.append("pword").append("=").append(myPWord).append("&");   // php 변수 앞에 '$' 붙이지 않는다
+            buffer.append("title").append("=").append(myTitle).append("&");           // 변수 구분은 '&' 사용
+            buffer.append("subject").append("=").append(mySubject);
+
+            OutputStreamWriter outStream = new OutputStreamWriter(http.getOutputStream(), "EUC-KR");
+            PrintWriter writer = new PrintWriter(outStream);
+            writer.write(buffer.toString());
+            writer.flush();
+            //--------------------------
+            //   서버에서 전송받기
+            //--------------------------
+            InputStreamReader tmp = new InputStreamReader(http.getInputStream(), "EUC-KR");
+            BufferedReader reader = new BufferedReader(tmp);
+            StringBuilder builder = new StringBuilder();
+            String str;
+            while ((str = reader.readLine()) != null) {       // 서버에서 라인단위로 보내줄 것이므로 라인단위로 읽는다
+                builder.append(str + "\n");                     // View에 표시하기 위해 라인 구분자 추가
+            }*/
+            Toast.makeText(OpenCloseActivity.this, "수동 개폐 완료", 0).show();
+        } catch (MalformedURLException e) {
+            //
+        } catch (IOException e) {
+            //
+        } // try
+    } // HttpPostData
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.cancel_action:
+                this.finish();
+                break;
+            case R.id.control_action:
+                HttpPostData();   // 서버와 자료 주고받기
+                break;
+        }
+    }
+} // Activity
